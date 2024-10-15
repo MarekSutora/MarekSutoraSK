11b21bc476d04d1484bf2ce6658a174fb7ce4342
Marek Šútora <marek.sutora@gmail.com>
2024-10-15 19:23:40 +0200

    small changes

--- src/components/Common/ShimmerButton.tsx
+++ src/components/Common/ShimmerButton.tsx
@@ -20,7 +20,7 @@ const ShimmerButton = ({
   return (
     <button
       className={cn(
-        "flex animate-shimmer flex-row items-center justify-center gap-1 rounded-md border bg-[linear-gradient(110deg,#1a1a1a,45%,#4a4a4a,55%,#1a1a1a)] bg-[length:300%_100%] p-1 text-sm font-medium text-gray-100 transition-colors hover:scale-105 focus:outline-none focus:ring-1 focus:ring-zinc-400/70 focus:ring-offset-1 focus:ring-offset-zinc-300/50 dark:border-neutral-900 dark:bg-[linear-gradient(110deg,#f5f5f5,45%,#cccccc,55%,#f5f5f5)] dark:bg-[length:300%_100%] dark:text-neutral-900",
+        "flex animate-shimmer text-nowrap flex-row items-center justify-center gap-1 rounded-md border bg-[linear-gradient(110deg,#1a1a1a,45%,#4a4a4a,55%,#1a1a1a)] bg-[length:300%_100%] p-1 text-sm font-medium text-gray-100 transition-colors hover:scale-105 focus:outline-none focus:ring-1 focus:ring-zinc-400/70 focus:ring-offset-1 focus:ring-offset-zinc-300/50 dark:border-neutral-900 dark:bg-[linear-gradient(110deg,#f5f5f5,45%,#cccccc,55%,#f5f5f5)] dark:bg-[length:300%_100%] dark:text-neutral-900",
         className,
       )}
       onClick={handleClick}
